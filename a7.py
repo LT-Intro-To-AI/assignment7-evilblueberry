87d153f1d648d2e5539ee1333038a9b4df6de6ae
evilblueberry <123481791+evilblueberry@users.noreply.github.com>
2023-04-26 15:12:55 -0500

    Update a7.py

--- a7.py
+++ a7.py
@@ -28,7 +28,7 @@ x_or_trainingdata = [
     ([1, 1], [0])
 ]
 
-xorn = NeuralNet(2, 20, 1)
+xorn = NeuralNet(2, 1, 1)
 
 xorn.train(x_or_trainingdata)
 
@@ -47,7 +47,7 @@ voter_opinion_data = [
     ([.6, .3, .4, .3, .6], [0])
 ]
 
-von = NeuralNet(5, 6, 1)
+von = NeuralNet(5, 1, 1)
 
 von.train(voter_opinion_data)
 
@@ -55,7 +55,15 @@ print(von.test_with_expected(voter_opinion_data))
 
 test_data = [
     [1, 1, 1, .1, .1],
-    [.5, .2, .1, .7, .7]
+    [.5, .2, .1, .7, .7],
+    [.8, .3, .3, .3, .8],
+    [.8, .3, .3, .8, .3],
+    [.9, .8, .8, .3, .6]
 ]
 
 print(f"case 1: {test_data[0]} evaluates to: {von.evaluate(test_data[0])}")
+print(f"case 1: {test_data[1]} evaluates to: {von.evaluate(test_data[1])}")
+print(f"case 1: {test_data[2]} evaluates to: {von.evaluate(test_data[2])}")
+print(f"case 1: {test_data[3]} evaluates to: {von.evaluate(test_data[3])}")
+print(f"case 1: {test_data[4]} evaluates to: {von.evaluate(test_data[4])}")
+
